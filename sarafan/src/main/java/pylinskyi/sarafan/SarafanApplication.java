6a8bf4d79030d84bf213f113a00bc31efc86f13f
k-pylinskyi <k.pylinskyi@gmail.com>
2022-03-09 21:56:09 +0100

    Sarfan init

---
+++ sarafan/src/main/java/pylinskyi/sarafan/SarafanApplication.java
@@ -0,0 +1,13 @@
+package pylinskyi.sarafan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarafanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SarafanApplication.class, args);
+	}
+
+}
---
+++ sarafan/src/test/java/pylinskyi/sarafan/SarafanApplicationTests.java
@@ -0,0 +1,13 @@
+package pylinskyi.sarafan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SarafanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
